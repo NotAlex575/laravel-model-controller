872b91d5dd3f0994812e75152d05409cd1b2d520
NotAlex575 <dapontealessandro89gmail.com>
2025-11-19 13:38:13 +0100

    creato layout + prova per la pagina listaFilm

--- resources/views/listaFilm.blade.php
+++ resources/views/listaFilm.blade.php
@@ -1,20 +1,29 @@
-@extends("layouts/master")
+@extends("layouts.master")
 
 @section("title")
     Lista film
 @endsection
 
-@section("main")
-    <div class="container">
-            <div class="row justify-content-center text-center">
-                @foreach ($films as $film)
-                    <x-card>
+@php
+    $films = ["Il Padrino", "Inception", "Titanic", "Interstellar"];
+@endphp
 
-                    </x-card>
-                @endforeach
-                <div class="col-md-8">
-                    <a href="{{ "listaFilm" }}" class="btn btn-primary btn-lg shadow">Vai alla lista dei film</a>
-                </div>
+@section("main")
+<main class="bg-light min-vh-100 py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            
+            @foreach ($films as $film)
+                <x-card>
+                    
+                </x-card>
+            @endforeach
+
+            <div class="col-12 text-center mt-4">
+                <a href="{{ url('listaFilm') }}" class="btn btn-success btn-lg shadow">Torna alla homepage!</a>
             </div>
+
         </div>
+    </div>
+</main>
 @endsection
